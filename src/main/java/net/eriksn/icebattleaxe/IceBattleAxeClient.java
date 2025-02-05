24ce6fa32132ab244150440494282329dd16c08d
Eriksn05 <stein05erik@gmail.com>
2025-02-05 19:13:05 +0100

    Starting Code

---
+++ src/main/java/net/eriksn/icebattleaxe/IceBattleAxeClient.java
@@ -0,0 +1,10 @@
+package net.eriksn.icebattleaxe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IceBattleAxeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
